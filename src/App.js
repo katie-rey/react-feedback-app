58a01ca4b42125479a227a8c4b1a46e22fa57b2f
Katie-Rey <katiejorey@gmail.com>
2022-06-14 10:52:54 +1200

    remove unused vars from site

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import FeedbackList from './components/FeedbackList'
 import FeedbackStats from './components/FeedbackStats'
 import FeedbackForm from './components/shared/FeedbackForm'
 import AboutPage from './pages/AboutPage'
 import AboutIconLink from './components/AboutIconLink'
-import Card from './components/shared/Card'
+
 import Post from './components/Post'
 import { FeedbackProvider } from './context/FeedbackContext'
 
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react'
-import { useState } from 'react'
 import Card from './shared/Card'
 import { FaTimes, FaEdit } from 'react-icons/fa'
 import FeedbackContext from '../context/FeedbackContext'
